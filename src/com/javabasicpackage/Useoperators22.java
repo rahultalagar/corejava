38a8187c88f4044e3f26a84adbbf3178a9e950c0
Rahul T R <rahultalagar@gmail.com>
2026-02-13 20:46:49 +0530

    Added 2nd java program  class

---
+++ src/com/javabasicpackage/Useoperators22.java
@@ -0,0 +1,19 @@
+package com.javabasicpackage;
+
+public class Useoperators22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n= 10;
+//		divisiblity checked by v(remainder ==0)
+		System.out.println(n % 3 ==0 && n % 4==0);
+		System.out.println("==============");
+		System.out.println("Eligiblity for marriage");
+		System.out.println("age :22 to 32");
+		int age =21;
+		System.out.println(age>=22 && age<= 32);
+		System.out.println("=====================");
+		
+
+	}
+}
